26dcc03806a0a66a70838aa5a399b31b7d3439c8
Dang Ton <dton@fastenal.com>
2018-12-26 16:33:01 -0600

    Solution
    Longest Sub-palindrome solution

--- MediumProblems/LongestSubString/Program.cs
+++ MediumProblems/LongestSubString/Program.cs
@@ -12,12 +12,12 @@ namespace LongestSubString
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
+            Console.WriteLine(LongestPalindrome("eabcb"));
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
-        public static int LengthOfLongestSubstring(string s)
+        /*public static int LengthOfLongestSubstring(string s)
         {
             int i = 0, res = 0;
             var mySet = new HashSet<char>();
@@ -44,6 +44,36 @@ namespace LongestSubString
                 i++;
             }
             return res;
+        }*/
+
+        public static string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return string.Empty;
+            int start = 0, end = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                var len1 = ExpandAroundCenter(s, i, i);
+                var len2 = ExpandAroundCenter(s, i, i + 1);
+                var len = Math.Max(len1, len2);
+                if (len > end - start)
+                {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+            return s.Substring(start, end-start+1);
+        }
+
+        private static int ExpandAroundCenter(string s, int left, int right)
+        {
+            int leftIdx = left, rightIdx = right;
+            var charArr = s.ToCharArray();
+            while (leftIdx >= 0 && rightIdx < s.Length && charArr[leftIdx] == charArr[rightIdx])
+            {
+                leftIdx--;
+                rightIdx++;
+            }
+            return rightIdx - leftIdx - 1;
         }
     }
 }
