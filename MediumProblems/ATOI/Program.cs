5cd4b550cf1fe8a0a40986f32b4c4881aa876bea
Dang Ton <dton@fastenal.com>
2018-12-27 17:07:00 -0600

    Atoi Problems
    The 116 ms solution to atoi c#

---
+++ MediumProblems/ATOI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATOI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Solution("-2147483649");
+            Console.WriteLine(res);
+
+            Console.Read();
+        }
+
+        public static int Solution(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return 0;
+            var trimString = str.Trim();
+            var charArr = trimString.ToCharArray();
+            if (charArr.Length == 0 || char.IsLetter(charArr[0]))
+                return 0;
+
+            var isNegative = charArr[0] == '-';
+            var idx = 0;
+            var res = 0;
+            if ((charArr[0] == '+' || charArr[0] == '-') && charArr.Length > 1) idx = 1;
+
+            while (idx < charArr.Length && char.IsDigit(charArr[idx]))
+            {
+                var convert = char.GetNumericValue(charArr[idx]);
+                if ((res > Int32.MaxValue / 10 || (res == Int32.MaxValue/ 10 && convert > 7) ) && !isNegative)
+                    return Int32.MaxValue;
+
+                if ((0 - res < Int32.MinValue / 10 || (0 - res == Int32.MinValue / 10 && convert > 7)) && isNegative)
+                    return  Int32.MinValue;
+
+                res = res * 10 + (int)convert;
+                idx++;
+            }
+
+            if (isNegative) res = 0 - res;
+
+            return res;
+        }
+    }
+}
---
+++ MediumProblems/ATOI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ATOI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATOI")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("673ba5c2-5c62-4d90-babf-b6c25ace7a05")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
